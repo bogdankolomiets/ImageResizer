2ef6141fc08a5f7db21e7e25baea1ca2198a1e61
Bogdan <kolom996@gmail.com>
2016-07-02 13:28:45 +0300

    Initial commit

---
+++ imageresizer/src/main/java/com/example/imageresizer/ImageResizer.java
@@ -0,0 +1,19 @@
+package com.example.imageresizer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+/**
+ * @author Bogdan Kolomiets
+ * @version 1
+ * @date 02.07.16
+ */
+public class ImageResizer {
+    private static float sCoeficient;
+
+    private ImageResizer() {
+
+    }
+    
+}
